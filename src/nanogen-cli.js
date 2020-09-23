f168112dd8259a3ea9d01e398fb8b444253161cf
Ahq <59189798+ahqcoding@users.noreply.github.com>
2020-09-23 20:11:14 +0800

    add build command

--- src/nanogen-cli.js
+++ src/nanogen-cli.js
@@ -4,15 +4,17 @@ const NanoGen = require("./nanogen");
 const program = new Command();
 const nanoGen = new NanoGen();
 
-program
-  .version("0.0.1")
-  .command("new <slug>")
-  .action(async function (slug) {
-    await nanoGen.collect();
-    await nanoGen.create(slug);
-  });
+program.command("new <slug>").action(async function (slug) {
+  await nanoGen.collect();
+  await nanoGen.create(slug);
+});
+
 program.command("serve").action(async function () {
   await nanoGen.watch();
+});
+
+program.command("build").action(async function () {
+  await nanoGen.build();
 });
 
 program.parse(process.argv);
